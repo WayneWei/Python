1e5adf5762dd18de4416ae825e4a8e55afbe4baf
Wayne Wei <tinglongwei@gmail.com>
2015-11-14 21:41:51 +0800

    Week 1 Beautiful Soup testing

---
+++ week1/week1.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+from BeautifulSoup import BeautifulSoup
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+url = "https://tw.stock.yahoo.com/us/worldidx.php"
+data = requests.get(url).text
+# print data
+soup = BeautifulSoup(data)
+
+# print (soup.prettify())
+
+table = soup('table')[5]
+
+# print table.prettify()
+
+tag = table.findAll('tr')[4].findAll('td')[2].b.string
+
+print tag
